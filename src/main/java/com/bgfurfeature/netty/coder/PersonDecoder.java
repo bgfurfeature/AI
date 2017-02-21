173c758f8cee5d73b5e26a3832028c3c8238c6bf
shanghai-Jerry <cjyou20110920sspu@163.com>
2017-02-21 09:50:46 +0800

    [ updated ]  add nio and netty

---
+++ src/main/java/com/bgfurfeature/netty/coder/PersonDecoder.java
@@ -0,0 +1,28 @@
+package com.bgfurfeature.netty.coder;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.handler.codec.ByteToMessageDecoder;
+
+import java.util.List;
+
+import com.bgfurfeature.netty.utils.ByteBufToBytes;
+import com.bgfurfeature.netty.utils.ByteObjConverter;
+ 
+ /**
+  * 反序列化
+  * 将Byte[]转换为Object
+  *
+  */
+public class PersonDecoder extends ByteToMessageDecoder {
+    @Override
+    protected void decode(ChannelHandlerContext ctx, ByteBuf in, List<Object> out) throws Exception {
+    	//工具类：将ByteBuf转换为byte[]
+        ByteBufToBytes read = new ByteBufToBytes();
+        byte[] bytes = read.read(in);
+        //工具类：将byte[]转换为object
+        Object obj = ByteObjConverter.byteToObject(bytes);
+        out.add(obj);
+    }
+ 
+}
