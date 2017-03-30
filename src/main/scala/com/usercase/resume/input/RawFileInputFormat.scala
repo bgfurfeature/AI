f3ecc6ecd570ebfa9c88560b454eee4e97a75e32
shanghai-Jerry <cjyou20110920sspu@163.com>
2017-03-30 10:34:16 +0800

    [added] 处理海量小文件的分片设置

--- src/main/scala/com/usercase/resume/input/RawFileInputFormat.scala
+++ src/main/scala/com/usercase/resume/input/RawFileInputFormat.scala
@@ -2,12 +2,14 @@ package com.usercase.resume.input
 
 import java.util
 
+import org.apache.hadoop.fs.Path
 import org.apache.hadoop.io.{BytesWritable, Text}
-import org.apache.hadoop.mapreduce.{InputSplit, RecordReader, TaskAttemptContext}
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat
+import org.apache.hadoop.mapreduce.{InputSplit, JobContext, RecordReader, TaskAttemptContext}
+import org.apache.hadoop.mapreduce.lib.input.{CombineFileRecordReader, CombineFileSplit, FileInputFormat}
 
 /**
   * Created by devops on 2017/3/28.
+  *
   */
 class RawFileInputFormat  extends  FileInputFormat[Text, BytesWritable] {
 
@@ -15,6 +17,12 @@ class RawFileInputFormat  extends  FileInputFormat[Text, BytesWritable] {
   RecordReader[Text, BytesWritable] = {
 
     new RawFileRecordReader()
+  }
+
+  // 每份简历文件不可被分片
+  override def isSplitable(context: JobContext, filename: Path): Boolean = {
+
+    false
 
   }
 
