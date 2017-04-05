0dfcce7640390e7276a514d57998d0c9cb9ab132
shanghai-Jerry <cjyou20110920sspu@163.com>
2017-04-05 19:20:48 +0800

    [added] use worksheet for code testing

--- src/main/scala/com/usercase/resume/ResumeSchedule.scala
+++ src/main/scala/com/usercase/resume/ResumeSchedule.scala
@@ -19,6 +19,40 @@ import scala.collection.mutable.ListBuffer
   */
 object ResumeSchedule  extends  CLogger {
 
+
+  val allFiles = new ListBuffer[String]
+
+  def getDirectorFiles(fs: FileSystem, path: String): Unit = {
+
+    val files = fs.listStatus(new Path(path))
+
+    val dirSize = files.filter(_.isDirectory).size
+
+    if(dirSize > 0) {
+
+     files.foreach { x =>
+
+       if(x.isDirectory) {
+
+        getDirectorFiles(fs, x.getPath.toString)
+
+       } else {
+
+        allFiles.+=(x.getPath.toString)
+
+       }
+
+     }
+    } else {
+
+      allFiles.+=(path)
+
+    }
+
+
+  }
+
+
   def postAndReturnString(client: HttpClient, url: String, body: String): String  = {
 
     try {
@@ -74,46 +108,58 @@ object ResumeSchedule  extends  CLogger {
     // val file = sc.newAPIHadoopRDD[Text, BytesWritable, RawFileInputFormat](conf, classOf[RawFileInputFormat], classOf[Text], classOf[BytesWritable])
 
     // new api in mapreduce.input...
-    val file = sc.newAPIHadoopFile[Text, BytesWritable, RawFileInputFormat](dir)
 
-    val files = file.map(_._1.toString).collect()
+    getDirectorFiles(fs, dir)
 
-    file.foreach { case(key, value) =>
+    println("allFileS: " + allFiles.size)
 
-      val reqObj = new JsonObject()
+    allFiles.foreach { path =>
 
-      if(key != null && value != null) {
+      println("dir: " + path)
 
-        val fileName = key.toString
-        val fileContent = value.getBytes
+      val file = sc.newAPIHadoopFile[Text, BytesWritable, RawFileInputFormat](path)
 
-        reqObj.put("name", fileName)
-        // reqObj.put("content", fileContent)
+      val files = file.map(_._1.toString).collect()
 
-        // "http://localhost:7778/api/extract_by_content"
-        val retVal = postAndReturnString(new HttpClient(new HttpClientParams), httpUrl, reqObj.encode())
+      file.foreach { case(key, value) =>
 
-        warnLog(logFileInfo, ("\"reqObj\":" + reqObj.toString + "," + "\"retVal\":" + retVal))
+        val reqObj = new JsonObject()
+
+        if(key != null && value != null) {
+
+          val fileName = key.toString
+          val fileContent = value.getBytes
+
+          reqObj.put("name", fileName)
+          reqObj.put("content", fileContent)
+
+          // "http://localhost:7778/api/extract_by_content"
+          val retVal = postAndReturnString(new HttpClient(new HttpClientParams), httpUrl, reqObj.encode())
+
+          warnLog(logFileInfo, ("\"reqObj\":" + reqObj.toString + "," + "\"retVal\":" + retVal))
+
+        }
+
 
       }
 
+      warn(s""""files":${files.size}""")
+
+      // 判断文件是否存在，存在则删除
+      files.foreach { fileName =>
+
+        val path = new Path(dst + "/" + fileName)
+
+        val isExist = fs.exists(path)
+
+        if(isExist) fs.deleteOnExit(path)
+
+      }
+
+      fs.close()
+
 
     }
-
-    warn(s""""files":${files.size}""")
-
-    // 判断文件是否存在，存在则删除
-    files.foreach { fileName =>
-
-      val path = new Path(dst + "/" + fileName)
-
-      val isExist = fs.exists(path)
-
-      if(isExist) fs.deleteOnExit(path)
-
-    }
-
-    fs.close()
 
 
   }
